8214369f691c58f788b2b9b61c24136341b9c057
ZARIFMAC <uzbzarif@gmail.com>
2022-01-05 14:29:18 +0900

    pygui

--- perception/screen.py
+++ perception/screen.py
@@ -1,4 +1,9 @@
 from PIL import ImageGrab
+import pyautogui
+
+#Get the list of windows title
+windows = pyautogui.getWindows()
+print(windows)
 img = ImageGrab.grab()
 #save the image
 img.save('screen.png')
