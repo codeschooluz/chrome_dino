e9eb42858181ecbc3a687d19889196266c85e85b
ZARIFMAC <uzbzarif@gmail.com>
2022-01-05 14:52:52 +0900

    get screen of dino window

--- perception/screen.py
+++ perception/screen.py
@@ -1,8 +1,22 @@
 from PIL import ImageGrab
 import pyautogui
+import pygetwindow as gw
 
 #Get the list of windows title
-windows = pyautogui.getWindows()
+windows = gw.getAllTitles()
+#Get dino window info
+#Get window name using hard code
+dino_win ='Google Chrome chrome://dino/'
+x1,y1,width,height = gw.getWindowGeometry(dino_win)
+x2 = x1 + width
+y2 = y1 + height
+#Define the dino bbox
+dino_bbox = (x1,y1,x2,y2)
+#Get the screen shot of dino window
+dino_screen = ImageGrab.grab(bbox=dino_bbox)
+#Save the screen shot
+dino_screen.save('dino_screen.png')
+
 print(windows)
 img = ImageGrab.grab()
 #save the image
