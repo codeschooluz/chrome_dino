fe6c717b9d20c93d72c4d6d041ce5c49922e1b8e
Jalilov <javohirjalilov777@gmail.com>
2022-01-06 15:08:53 +0500

    get window geometry. wnck python library. suppport windows and Linux

--- perception/screen.py
+++ perception/screen.py
@@ -1,19 +1,46 @@
 from PIL import ImageGrab
 import pyautogui
-import pygetwindow as gw
+import sys
+import os
 
-#Get the list of windows title
-windows = gw.getAllTitles()
-#Get dino window info
-#Get window name using hard code
-dino_win ='Google Chrome chrome://dino/'
-x1,y1,width,height = gw.getWindowGeometry(dino_win)
-x2 = x1 + width
-y2 = y1 + height
-#Define the dino bbox
-dino_bbox = (x1,y1,x2,y2)
-#Get the screen shot of dino window
-dino_screen = ImageGrab.grab(bbox=dino_bbox)
-#Save the screen shot
-dino_screen.save('dino_screen.png')
+if sys.platform in ['win32', 'win64', 'windows']:
+    import pygetwindow as gw
+    #Get the list of windows title
+    windows = gw.getAllTitles()
+    #Get dino window info
+    #Get window name using hard code
+    dino_win ='Google Chrome chrome://dino/'
+    x1,y1,width,height = gw.getWindowGeometry(dino_win)
+    x2 = x1 + width
+    y2 = y1 + height
+    #Define the dino bbox
+    dino_bbox = (x1,y1,x2,y2)
+    #Get the screen shot of dino window
+    dino_screen = ImageGrab.grab(bbox=dino_bbox)
+    #Save the screen shot
+    dino_screen.save('dino_screen.png')
 
+elif sys.platform in ['linux', 'linux2']:
+    try:
+        import pgi
+        pgi.require_version('Wnck', '3.0')
+        from pgi.repository import Wnck
+    except:
+        print('Please install Wnck')
+        Wnck = None
+    if Wnck is not None:
+        for i in range(100):
+            screen = Wnck.Screen.get_default()
+            screen.force_update()
+            windows = screen.get_windows()
+            for window in windows:
+                if window.get_name() == 'chrome://dino/ - Google Chrome':
+                    x1,y1,width,height = window.get_geometry()
+                    x2 = x1 + width
+                    y2 = y1 + height
+                    dino_bbox = (x1,y1,x2,y2)
+                    dino_screen = ImageGrab.grab(bbox=dino_bbox)
+                    image = f'screen_{i}.png'
+                    dino_screen.save(image)
+                    # os.remove(image)
+                    break
