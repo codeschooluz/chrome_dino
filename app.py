d831bea3a945ffa4e4b3ac6f220c0bf19cf26389
Jalilov <javohirjalilov777@gmail.com>
2022-01-16 18:47:25 +0500

    dino controller

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from perception.screen import getChromeArea,getCanvasArea
+from tracemalloc import stop
+from perception.screen import getChromeArea,getCanvasArea, getDino
 from PIL import ImageGrab
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import pyautogui
+
+fence_color = (83, 83, 83)
+
 x,y,w,h=getChromeArea(False)
-#Define the loop for the game
-#Get canvas
-x1,y1,x2,y2=getCanvasArea(x,y,w,h)
+thresh_img = getCanvasArea(x,y,w,h)
+x1,y1,w1,h1 = getDino(thresh_img)
+
 while True:
-    
+    image = ImageGrab.grab()
+    fence_pixel = image.getpixel((w//4+w1//2,y1+h1//2))
+    # print(fence_pixel)
+    if fence_pixel == fence_color:
+        print("jump")
+        pyautogui.press('space')
     #Take the screenshot
-    img = ImageGrab.grab(bbox=(x1,y1,x2,y2))
-    #Convert img to numpy array
-    rgb_img = np.array(img).astype(np.uint8)
+    rgb_img = np.array(image).astype(np.uint8)
+    rgb_img = cv2.rectangle(rgb_img,(x1,y1),(x1+w1,y1+h1),(255,0,0),2)
     #Imshow the image
-    cv2.imshow('image',rgb_img)
+    # cv2.imshow('image',rgb_img)
     #Wait for key press
     k = cv2.waitKey(1) & 0xFF
     print(k)
